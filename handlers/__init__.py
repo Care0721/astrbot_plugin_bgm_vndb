b4fbb7359a4fb368bec799d5d94a99c363be32c8
Care0721 <3524815759@qq.com>
2026-04-03 17:51:33 +0800

    创建 handlers/__init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,6 @@
+"""处理器初始化"""
+from .gal_news import GalNewsHandler
+from .preorder import PreorderHandler
+from .patch_progress import PatchProgressHandler
+
+__all__ = ['GalNewsHandler', 'PreorderHandler', 'PatchProgressHandler']
