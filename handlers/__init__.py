2efc94fd759be633beddf29bb1602b95ecfaeb7b
Care0721 <3524815759@qq.com>
2026-04-03 17:53:47 +0800

    删除 __init__.py

--- handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""处理器初始化"""
-from .gal_news import GalNewsHandler
-from .preorder import PreorderHandler
-from .patch_progress import PatchProgressHandler
-
-__all__ = ['GalNewsHandler', 'PreorderHandler', 'PatchProgressHandler']
