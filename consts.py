8f4ef63a1c418dce2a61527debf1e66a89cf9fe8
Care0721 <3524815759@qq.com>
2026-04-03 18:04:15 +0800

    删除 consts.py

--- consts.py
+++
@@ -1,5 +0,0 @@
-BANGUMI_API_URL = "https://api.bgm.tv"
-BANGUMI_TOKEN = "McT3CzkJaKQs45WYRfhUU0oB8ejvE1Aj5WGYLm2J"
-
-VNDB_API_URL = "https://beta.vndb.org/api/kana"
-VNDB_TOKEN = "zkxy-bidaq-8coke-gzoy-gdem1-9zscs-kcdf"
