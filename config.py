3109d3438533dc6b259ef2b32c76b4644bc53227
Care0721 <3524815759@qq.com>
2026-04-03 17:50:29 +0800

    创建 config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+"""
+配置文件
+"""
+
+# API 配置
+BANGUMI_API_BASE = "https://api.bgm.tv"
+BANGUMI_API_KEY = "McT3CzkJaKQs45WYRfhUU0oB8ejvE1Aj5WGYLm2J"
+
+VNDB_API_BASE = "https://beta.vndb.org/api"
+VNDB_API_KEY = "zkxy-bidaq-8coke-gzoy-gdem1-9zscs-kcdf"
+
+# 定时任务配置
+UPDATE_INTERVAL = 3600  # 每小时检查一次
+
+# 分类 ID（Bangumi）
+GALGAME_TYPE_ID = 2  # Visual Novel/Galgame
+
+# 存储配置
+STORAGE_PATH = "./data/gal_news"
